01cc94eed165ca041be6e2f2657432620c134984
Endre Simo <esimovaq@yahoo.com>
2017-10-31 22:28:49 +0200

    Refactoring settings component

--- src/components/settings.js
+++ src/components/settings.js
@@ -3,30 +3,15 @@ import RaisedButton from 'material-ui/RaisedButton';
 import Slider from 'material-ui/Slider';
 import FontIcon from 'material-ui/FontIcon';
 
-const sliderList = [
-  {  
-    name: "Blur Radius",
-    title: "Blur Radius:",
-    range: {
-      min: 0,
-      max: 20,
-      default: 4,
-      step: 1
-    }
-  },
-  {  
-    name: "Sobel Threshold",
-    title: "Sobel Threshold:",
-    range: {
-      min: 2,
-      max: 50,
-      default: 10,
-      step: 1
+export default class Settings extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      sliders: []
     }
   }
-]
 
-export default class Settings extends Component {    
   mainStyle = {
     display: 'flex',    
     flexDirection: 'row',
@@ -40,46 +25,64 @@ export default class Settings extends Component {
     width: 400,
   }
 
-  state = {
-    slider: 10
-  }
-
-  handleSlider = (event, value) => {    
+  handleSlider = (event, value) => {
     this.setState({
       slider: value
     });
   };
 
-  render() {    
-    return (
-      <div className="Settings">          
-        {sliderList.map(slider => {                    
-          <div style={this.mainStyle}>    
-            <span style={this.textStyle}>slider.title <Value sliderValue={this.state.slider} /></span>                      
-            <SliderItem name={slider.name}
-              min={slider.range.min} 
-              max={slider.range.max} 
-              default={slider.range.default} 
-              step={slider.range.step} 
-              style={this.sliderStyle}
-            />
-          </div> 
-        })}               
-      </div>
-    );
+  componentWillMount() {
+    this.setState({
+      sliders : [
+        {
+          name: "Blur Radius",
+          title: "Blur Radius:",
+          range: {
+            min: 0,
+            max: 20,
+            default: 4,
+            step: 1
+          }
+        },
+        {
+          name: "Sobel Threshold",
+          title: "Sobel Threshold:",
+          range: {
+            min: 2,
+            max: 60,
+            default: 10,
+            step: 1
+          }
+        }
+      ]
+    });
   }
-}
 
-export class SliderItem extends Component {
+  componentDidUpdate(prevProps, prevState) {
+    console.log(prevState);
+  }
+
   render() {
-    return (      
-      <Slider name={this.props.name}
-        min={this.props.min} 
-        max={this.props.max} 
-        value={this.props.default} 
-        step={this.props.step} 
-        style={this.props.style} 
-        onChange={this.handleSlider.bind(this)} axis="x" />
+    let sliderItems;
+    sliderItems = this.state.sliders.map(slider => {
+      return (
+        <div style={this.mainStyle} key={slider.name} >
+          <span style={this.textStyle}>{slider.title} <Value sliderValue={this.state.slider} /></span>
+          <Slider name={slider.name}
+            min={slider.range.min}
+            max={slider.range.max}
+            value={slider.range.default}
+            step={slider.range.step}
+            style={this.sliderStyle}
+            onChange={this.handleSlider}
+          />
+        </div>
+      );
+    });
+    return (
+        <div className="Settings">
+          {sliderItems}
+        </div>
     );
   }
 }
